c1dd5b1bca5da1f9c711593f1189d6f7e89e7841
Adler <royadler@outlook.com>
2019-07-28 14:55:27 +0200

    Exercise is now clickable

--- lib/exercise_view.dart
+++ lib/exercise_view.dart
@@ -8,6 +8,21 @@ class ExerciseView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(exercise.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(32),
+            height: 150.0,
+            color: Colors.green,
+          ),
+          Text("Sets: " + exercise.sets.toString()),
+          Text("Reps" + exercise.reps.toString()),
+        ],
+      ),
+    );
   }
 }
