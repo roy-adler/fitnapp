c578024be878eef8beec67b59b6ef7d6bffc6e83
Adler <royadler@outlook.com>
2019-07-28 14:39:10 +0200

    ExerciseView created and set

--- lib/exercise.dart
+++ lib/exercise.dart
@@ -1,3 +1,4 @@
+import 'package:fitnapp/exercise_view.dart';
 import 'package:flutter/material.dart';
 
 class Exercise extends StatelessWidget {
@@ -5,11 +6,20 @@ class Exercise extends StatelessWidget {
   int sets;
   int reps;
 
-
   Exercise({@required this.title, this.sets = 3, this.reps = 12});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListTile(
+      title: Text(title),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ExerciseView(
+            exercise: Exercise(title: title, sets: sets, reps: reps),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/exercise_view.dart
+++ lib/exercise_view.dart
@@ -1,6 +1,10 @@
+import 'package:fitnapp/exercise.dart';
 import 'package:flutter/material.dart';
 
 class ExerciseView extends StatelessWidget {
+  Exercise exercise;
+
+  ExerciseView({this.exercise});
 
   @override
   Widget build(BuildContext context) {
