e788cf40a6ef8684e89299e3be0dbf2acaacf6cc
Adler <royadler@outlook.com>
2019-07-28 17:40:46 +0200

    Adding DataFile

---
+++ lib/Data/Data.dart
@@ -0,0 +1,8 @@
+import 'package:fitnapp/App/Fitness/fitness_plan.dart';
+import 'package:fitnapp/App/Fitness/fitness_plan_list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:statusbar/statusbar.dart';
+
+import 'package:fitnapp/App/Exercise/exercise.dart';
+
+class Data {}
